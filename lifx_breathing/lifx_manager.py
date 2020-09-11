126535f6ba8d46c2c7c569537dbd5710407a7b4e
Asim Ihsan <asim.ihsan@gmail.com>
2020-09-10 21:04:55 -0700

    Background refresh lights, better refreshing

--- lifx_breathing/lifx_manager.py
+++ lifx_breathing/lifx_manager.py
@@ -11,6 +11,7 @@ from pprint import pprint
 from typing import List, Set, FrozenSet, Optional, Any
 import dataclasses
 import logging
+import threading
 import time
 
 # -----------------------------------------------------------------------------
@@ -49,48 +50,67 @@ class LifxLightWrapper:
 class LifxManager:
     UPDATE_LIGHTS_ITERATIONS: int = 3
     UPDATE_LIGHTS_SLEEP_INTERVAL_SECONDS: float = 0.017
+    BACKGROUND_LIGHTS_UPDATE_INTERVAL_SECONDS: float = 15 * 60
 
     _lan: LifxLAN
     _lights: List[LifxLightWrapper]
+    _updater_thread: threading.Timer
 
     def __init__(self) -> None:
+        logger.info("__init__ entry")
         self._lan = LifxLAN()
         self._lights = list()
+        self.update_lights_in_thread()
 
     @property
     def lights(self) -> List[LifxLightWrapper]:
         return self._lights
 
     def get_light(self, location: str, label: str) -> Optional[LifxLightWrapper]:
+        logger.info("get_light entry")
         for light in self._lights:
             if light.location == location and light.label == label:
                 return light
         return None
 
+    def update_lights_in_thread(self) -> None:
+        self.update_lights()
+        self._updater_thread = threading.Timer(
+            self.BACKGROUND_LIGHTS_UPDATE_INTERVAL_SECONDS, self.update_lights_in_thread
+        )
+        self._updater_thread.start()
+
     def update_lights(self) -> None:
+        logger.info("update_lights entry")
         self._lights = self.get_new_lights()
 
     def get_new_lights(self) -> List[LifxLightWrapper]:
+        logger.info("get_new_lights entry")
+        seen_mac_addresses: Set[str] = set()
         result: Set[LifxLightWrapper] = set()
         for i in range(self.UPDATE_LIGHTS_ITERATIONS):
             time.sleep(self.UPDATE_LIGHTS_SLEEP_INTERVAL_SECONDS)
             devices: List[Device] = self._lan.get_devices()
-            try:
-                current_lights: List[LifxLightWrapper] = [
-                    LifxLightWrapper(
+            current_lights: List[LifxLightWrapper] = []
+            for device in devices:
+                if device.get_mac_addr() in seen_mac_addresses:
+                    continue
+                try:
+                    light: LifxLightWrapper = LifxLightWrapper(
                         location=device.get_location_label(),
                         label=device.get_label(),
                         ip_address=device.get_ip_addr(),
                         mac_address=device.get_mac_addr(),
                     )
-                    for device in devices
-                ]
-            except lifxlan.errors.WorkflowException:
-                logger.exception("error while updating lights")
-                continue
+                    current_lights.append(light)
+                except lifxlan.errors.WorkflowException:
+                    logger.exception("error while updating lights")
+                    continue
+                seen_mac_addresses.add(device.get_mac_addr())
             result.update(current_lights)
         return sorted(result, key=lambda x: (x.location, x.label))
 
     def close(self) -> None:
-        pass
+        logger.info("close entry")
+        self._updater_thread.cancel()
 
